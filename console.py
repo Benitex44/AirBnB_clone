be1ff2f99f487e72a19245428463183a212b9dc4
Benitex44 <benbenedict971@gmail.com>
2022-08-07 19:52:34 +0100

    added default

--- console.py
+++ console.py
@@ -178,7 +178,7 @@ class HBNBCommand(cmd.Cmd):
             try:
                 setattr(clschange, attributename, float(updatevalue))
                 storage.save()
-            except NameError:
+            except:
                 setattr(clschange, attributename, str(updatevalue))
                 storage.save()
 
